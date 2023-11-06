c3966857b53ec1c6be77e7eeb866928705c3d5e2
Mahmoud <mahmoud@localhost.localdomain>
2023-11-06 08:58:14 +0200

    remove sudo

--- server.sh
+++ server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 container_name="apache-server"
-if sudo docker ps -a --format "{{.Names}}" | grep -q "$container_name"; then
-    sudo docker stop "$container_name" && sudo docker rm "$container_name"
+if docker ps -a --format "{{.Names}}" | grep -q "$container_name"; then
+    docker stop "$container_name" && sudo docker rm "$container_name"
 fi
 
-sudo docker build -t apache-server .
-sudo docker run -d --name apache-server -p 8899:80 apache-server
+docker build -t apache-server .
+docker run -d --name apache-server -p 8899:80 apache-server
