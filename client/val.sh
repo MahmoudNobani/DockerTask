31c22cdc4c24d1964ee12acaddf536f4cc55392c
Mahmoud <mahmoud@localhost.localdomain>
2023-11-07 10:32:27 +0200

    editing tests to fail

--- client/val.sh
+++ client/val.sh
@@ -4,13 +4,16 @@ container_name="client-server"
 
 sudo docker exec "$container_name" /bin/bash -c '
 if yum info httpd &>/dev/null; then
-    if yum info httpd | grep -qi "From repo   : MAINserver"; then
+    if yum info httpd | grep -qi "From repo   : MAINerver"; then
         echo "true"
+        exit 0
     else
         echo "false1"
+        exit 1
     fi
 else
     echo "false2"
+    exit 2
 fi
 '
 
--- test.sh
+++ test.sh
@@ -5,7 +5,9 @@ container_name="apache-server"
 docker exec "$container_name" /bin/bash -c '
 if yum info httpd &>/dev/null; then
     echo "true"
+    exit 0
 else
     echo "false2"
+    exit 0
 fi
 '
